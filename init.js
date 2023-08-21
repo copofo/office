0d7d829608031e388e993ae1c88bb05f26592cde
copofo <121464011+copofo@users.noreply.github.com>
2023-08-20 22:09:12 -0300

    primeiros arquivos

---
+++ init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFNMjOUZehRac2nOrz3opxY5_PYYB71yU",
+    authDomain: "office-salas-agenda.firebaseapp.com",
+    projectId: "office-salas-agenda",
+    storageBucket: "office-salas-agenda.appspot.com",
+    messagingSenderId: "682455108646",
+    appId: "1:682455108646:web:4e9172f972a1007210eae7",
+    measurementId: "G-1XRT0RPRNN"
+};
+
+firebase.initializeApp(firebaseConfig);
